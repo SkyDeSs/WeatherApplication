a929ea78273055d9f649a4ae30a50e03043a27d6
Denys Zemliakov <denisenko3333@gmail.com>
2022-12-21 14:08:11 +0200

    public API key was hidden, changed UI

--- app/src/main/java/com/example/weather_application/MainActivity.kt
+++ app/src/main/java/com/example/weather_application/MainActivity.kt
@@ -2,42 +2,31 @@ package com.example.weather_application
 
 import android.os.AsyncTask
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
-import android.widget.ProgressBar
-import android.widget.RelativeLayout
-import android.widget.TextView
-import com.example.weather_application.databinding.ActivityMainBinding
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import org.json.JSONObject
 import java.net.URL
 import java.text.SimpleDateFormat
 import java.util.*
 
+
 class MainActivity : AppCompatActivity() {
-    val City: String = "Chernihiv, UA"
-    val API: String = "19af5c01c96b0b8f3152057429f79fd7"
+    var City: String = "Chernihiv, UA"
+    val API = BuildConfig.WEATHER_API
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        findViewById<Button>(R.id.search_button).setOnClickListener {
+            City = findViewById<EditText>(R.id.location).text.toString()
+            weatherTask().execute()
+        }
         weatherTask().execute()
     }
+
     inner class weatherTask():AsyncTask<String, Void, String>()
     {
-        override fun onPreExecute() {
-            super.onPreExecute()
-            findViewById<ProgressBar>(R.id.loader).visibility = View.VISIBLE
-            findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.GONE
-            findViewById<TextView>(R.id.error_text).visibility = View.GONE
-        }
 
         override fun doInBackground(vararg p0: String?): String? {
             var response:String?
@@ -62,15 +51,15 @@ class MainActivity : AppCompatActivity() {
                 val wind = JSONObj.getJSONObject("wind")
                 val weather = JSONObj.getJSONArray("weather").getJSONObject(0)
                 val updatedAt:Long = JSONObj.getLong("dt")
-                val updatedAtText = "Updated at: "+ SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(Date(updatedAt*1000))
+                val updatedAtText = "Updated at: "+ SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.ENGLISH).format(Date(updatedAt*1000))
                 val temperature = main.getString("temp")+"°C"
                 val temperatureMin = "Min Temp: " + main.getString("temp_min")+"°C"
                 val temperatureMax = "Max Temp: " + main.getString("temp_max")+"°C"
-                val pressure = main.getString("pressure")
-                val humidity = main.getString("humidity")
+                val pressure = main.getString("pressure") + " hPa"
+                val humidity = main.getString("humidity") + '%'
                 val sunrise:Long = sys.getLong("sunrise")
                 val sunset:Long = sys.getLong("sunset")
-                val windSpeed = wind.getString("speed")
+                val windSpeed = wind.getString("speed") + " m/s"
 
                 val weatherDescription = weather.getString("description")
                 val address = JSONObj.getString("name")+", "+sys.getString("country")
@@ -88,12 +77,11 @@ class MainActivity : AppCompatActivity() {
                 findViewById<TextView>(R.id.pressure).text = pressure
                 findViewById<TextView>(R.id.humidity).text = humidity
 
-                findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
                 findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.VISIBLE
             }
             catch (e: Exception) {
-                findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
-                findViewById<TextView>(R.id.error_text).visibility = View.VISIBLE
+                val toast: Toast = Toast.makeText(applicationContext, "Incorrect city", 5000)
+                toast.show()
             }
         }
     }
